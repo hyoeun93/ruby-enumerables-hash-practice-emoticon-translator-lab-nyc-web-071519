845f6ce5cf5dca76e126461c595a14190b372d96
HyoEun Lee <hyoeun93@gmail.com>
2019-07-06 18:41:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(path)
   
   emoticons.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = emglish_word 
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word 
   end 
   emoticon_hash
 end
